7330ec9212e1d34bb2e594a1a9598f7983a96c6b
Morozov312 <alexander.morozov312@gmail.com>
2020-12-15 23:30:48 +0700

    update js

--- script/burger.js
+++ script/burger.js
@@ -1,13 +1,13 @@
-var burger = document.querySelector('#burger');
-var bgc = document.querySelector('#welcome');
-var button = document.querySelector('#welcome_button');
-var nav = document.querySelector('#navigation');
-var h1 = document.querySelector('#h1');
+let burger = document.querySelector('#burger');
+let bgc = document.querySelector('#welcome');
+let button = document.querySelector('#welcome_button');
+let nav = document.querySelector('#navigation');
+let h1 = document.querySelector('#h1');
 
-burger.onclick = function(){
+burger.addEventListener('click', () => {
     burger.classList.toggle('active_burger');
     nav.classList.toggle('active_menu');
     bgc.classList.toggle('active_bgc');
     button.classList.toggle('hide');
     h1.classList.toggle('hide');
-}
+})
